dee4b8ffc16e8d789cb08d2b215a96c17cafadcc
Anuj Dwivedi <anuj75dwivedi17@gmail.com>
2021-08-17 16:21:48 +0530

    init

---
+++ Linked_list_prog.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Linked_list_prog {
+    public static void main(String args[]) {
+        System.out.println("Name of Cars: ");
+        LinkedList ll = new LinkedList<String>();
+        ll.add("i20");
+        System.out.println("Name of Cars: " + ll);
+    }
+}

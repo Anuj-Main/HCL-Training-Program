056bd85e33a075af5abe20ea7f9f7f7740a7e64f
Anuj Dwivedi <anuj75dwivedi17@gmail.com>
2021-08-17 18:32:27 +0530

    init

--- Vehicle.java
+++ Vehicle.java
@@ -2,14 +2,15 @@ abstract class Vehicle{
     private String vehicle_name;
     private String vehicle_ownersname;
     private int vehicle_number;
-    public Vehicle(String vehicle_name, String vehicle_ownersname, int vehicle_number){
+
+    public Vehicle(String vehicle_name, String vehicle_ownersname, int vehicle_number) {
         System.out.println("Vehicle Details");
         this.vehicle_name = vehicle_name;
         this.vehicle_ownersname = vehicle_ownersname;
-        this.vehicle_number = vehicel_number;
+        this.vehicle_number = vehicle_number;
     }
     public void check_details(){
-        System.out.println("Check Details to " + this.vehicle_name + " " + this.vehicle_ownersname);
+        System.out.println("Check Details to " + this.vehicle_name + " " + this.vehicle_ownersname+" " +this.vehicle_number);
     }
 }
 public class Main extends Vehicle{

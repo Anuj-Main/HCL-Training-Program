d9f41f6292a3feb055fb54c71c3843e1eda90217
Anuj Dwivedi <anuj75dwivedi17@gmail.com>
2021-08-18 10:11:48 +0530

    init

--- Linked_list_prog.java
+++ Linked_list_prog.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Linked_list_prog {
     public static void main(String args[]) {
         System.out.println("Name of Cars: ");
-        LinkedList ll = new LinkedList<String>();
+        LinkedList<String> ll = new LinkedList<String>();
         ll.add("i20");
         ll.add("Amaze");
         ll.add("Santro");
